e8212223357228e6a8ac78ab9c57f05709ac6a80
Roy Doyal <rldoyal@hotmail.com>
2018-09-06 22:49:16 -0700

    Initial commit

---
+++ Aletto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2_598_Aletto_Doyal
+{
+    class Aletto
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P2_598_Aletto_Doyal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
